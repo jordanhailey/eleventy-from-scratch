3df17605e1d3c741b4fcf484775f16b9410d787a
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-06-28 17:02:39 -0700

    adding js-yaml

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
+const yaml = require("js-yaml");
 module.exports = (config) => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
+  config.addDataExtension("yaml", contents => yaml.load(contents));
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
