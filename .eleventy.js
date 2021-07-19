1433406d2623bdf1fc77d53d08b952a3b0b02e07
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-07-19 11:53:49 -0700

    utilize collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
 const yaml = require("js-yaml");
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 module.exports = (config) => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
   config.addDataExtension("yaml", contents => yaml.load(contents));
+
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'))
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(x => x.data.featured)
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
