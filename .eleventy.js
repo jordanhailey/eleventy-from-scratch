eb7a26c5aab815ee642210b83f03c5a5303ee04a
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-08-02 21:18:19 -0700

    posts layout and filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 const yaml = require("js-yaml");
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
 module.exports = (config) => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
@@ -14,10 +19,14 @@ module.exports = (config) => {
   config.addCollection('featuredWork', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(x => x.data.featured)
   });
+
   // Returns a collection of blog posts in reverse date order
   config.addCollection('blog', collection => {
     return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
   });
+
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
 
   return {
     markdownTemplateEngine: 'njk',
